27ae7ca8f837e27b0bb7ddefd8c1e6ad860d6e7d
Sanjeev <sanjeeva3101@gmail.com>
2026-06-25 16:31:44 +0530

    Remove broken image imports from ProductDetails

--- celesse-client/src/pages/ProductDetails.jsx
+++ celesse-client/src/pages/ProductDetails.jsx
@@ -1,19 +1,5 @@
 import { useState } from "react";
 
-// ✅ IMAGE IMPORTS
-import p1 from "../assets/p1.jpg";
-import p2 from "../assets/p2.jpg";
-import p3 from "../assets/p3.jpg";
-import p4 from "../assets/p4.jpg";
-import p5 from "../assets/p5.jpg";
-import p6 from "../assets/p6.jpg";
-import p7 from "../assets/p7.jpg";
-import p8 from "../assets/p8.jpg";
-import p9 from "../assets/p9.jpg";
-
-// ✅ IMAGE MAP
-const imageMap = { p1, p2, p3, p4, p5, p6, p7, p8, p9 };
-
 function ProductDetails({ product, addToCart, setSelectedPage }) {
   const [quantity, setQuantity] = useState(1);
 
@@ -35,7 +21,7 @@ function ProductDetails({ product, addToCart, setSelectedPage }) {
         {/* IMAGE */}
         <div>
           <img
-            src={imageMap[product.image] || product.image}
+            src={product.image}
             alt={product.title}
             className="w-full h-[450px] object-cover rounded-lg shadow-lg"
           />
